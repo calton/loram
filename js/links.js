820602c4a4a94571f70f09ab355b111c3bad3f16
calton <christopher.alton@gmail.com>
2016-05-27 12:43:49 -0500

    Initial Upload

---
+++ js/links.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#square1").attr("href", "home.html");
+	$("#square2").attr("href", "home.html");
+	$("#square3").attr("href", "expanding.html");
+	$("#square4").attr("href", "home.html");
+	
+	$("#img1").attr("src", "./img/img2.png");
+	$("#img2").attr("src", "./img/img1.png");
+	$("#img3").attr("src", "./img/img4.png");
+	$("#img4").attr("src", "./img/img3.png");
+});
